9d3df49f53deb0719020bc3edaec66cf700e8a25
Nitika Sehgal <62950811+nitikasehgal@users.noreply.github.com>
2023-01-09 18:56:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Clima extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: LoadingScreen(),
     );
